9f6306b437dc11c39b064769ba79434b6b24fd8c
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-10-26 10:40:59 +0100

    chore(red-cli): upgrade to 0.0.8

--- Formula/red-cli.rb
+++ Formula/red-cli.rb
@@ -1,30 +1,30 @@
 class RedCli < Formula
   desc "Red, a TUI client for Redis"
   homepage "https://github.com/evertdespiegeleer/red-cli"
-  version "0.0.7"
+  version "0.0.8"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/evertdespiegeleer/red-cli/releases/download/v#{version}/red-darwin-arm64"
-      sha256 "f5252f55eb9f3b4982d5bf7dc2286fc215af164fd8b727da28f40b14741431e1"
+      sha256 "f419b983a9d15fa3f9aa66b76d64805e11816f12589213d2e3557b63fe791084"
     end
 
     on_intel do
       url "https://github.com/evertdespiegeleer/red-cli/releases/download/v#{version}/red-darwin-x64"
-      sha256 "f5252f55eb9f3b4982d5bf7dc2286fc215af164fd8b727da28f40b14741431e1"
+      sha256 "f419b983a9d15fa3f9aa66b76d64805e11816f12589213d2e3557b63fe791084"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/evertdespiegeleer/red-cli/releases/download/v#{version}/red-linux-x64"
-      sha256 "82539adf4d5a1533629dd787ce63aee93771e2bad2096dbd268fa02fe4b17143"
+      sha256 "926973fe17b5045c51f3791a27d72af19151e403040bdd5ceafb0c551781feca"
     end
 
     on_arm do
       url "https://github.com/evertdespiegeleer/red-cli/releases/download/v#{version}/red-linux-arm64"
-      sha256 "82539adf4d5a1533629dd787ce63aee93771e2bad2096dbd268fa02fe4b17143"
+      sha256 "926973fe17b5045c51f3791a27d72af19151e403040bdd5ceafb0c551781feca"
     end
   end
 
